4f90298f2133425e0c31d1a26a106587ed5ce91d
wuhuai666 <3331670554@qq.com>
2021-06-10 14:24:35 +0800

    第一次修改

---
+++ README.md
@@ -0,0 +1,110 @@
+# 图书馆管理系统
+
+### 概述
+基于Spring + Spring MVC + MyBatis的图书馆管理系统，使用Maven进行包管理。主要功能包括：图书查询、图书管理、图书编辑、读者管理、图书的借阅与归还以及借还日志记录等。
+
+### 环境配置
+#### 开发环境：Windows 10，IntelliJ IDEA 2018.3
+#### 运行配置
+1. 首先安装Mysql5.7，设置用户名为root，密码为123456，并保证其在运行状态，并执行library.sql文件导入数据。
+2. 然后再配置Maven到环境变量中，在源代码目录下运行
+```sh
+# mvn jetty:run
+```
+3. 使用浏览器访问http://localhost:8080即可进入系统。
+
+### 概念设计
+用户分为两类：读者、图书馆管理员。图书馆管理员可以修改读者信息，修改书目信息，查看所有借还日志等；读者仅可以修改个人信息、借阅或归还书籍和查看自己的借还日志。
+<img src="./preview/1.png" style="width: 50%"><img src="./preview/2.png" style="width: 50%;float: right">
+
+#### 数据库E-R图
+<img src="./preview/3.png">
+
+### 逻辑设计
+共有6个表：
+
+#### 1. 图书书目表book_info
+| 名           | 类型    | 长度 | 小数点 | NULL | 用途     | 键   |
+| :----------- | :------ | ---- | ------ | ---- | -------- | ---- |
+| book_id      | bigint  | 20   | 0      | 否   | 图书号   | ✔    |
+| name         | varchar | 20   | 0      | 否   | 书名     |      |
+| author       | varchar | 15   | 0      | 否   | 作者     |      |
+| publish      | varchar | 20   | 0      | 否   | 出版社   |      |
+| ISBN         | varchar | 15   | 0      | 否   | 标准书号 |      |
+| introduction | text    | 0    | 0      | 是   | 简介     |      |
+| language     | varchar | 4    | 0      | 否   | 语言     |      |
+| price        | decimal | 10   | 2      | 否   | 价格     |      |
+| pub_date     | date    | 0    | 0      | 否   | 出版时间 |      |
+| class_id     | int     | 11   | 0      | 是   | 分类号   |      |
+| number       | int     | 11   | 0      | 是   | 剩余数量 |      |
+
+#### 2. 数据库管理员表admin
+| 名       | 类型    | 长度 | 小数点 | NULL | 用途   | 键   |
+| :------- | :------ | ---- | ------ | ---- | ------ | ---- |
+| admin_id | bigint  | 20   | 0      | 否   | 账号   | ✔    |
+| password | varchar | 15   | 0      | 否   | 密码   |      |
+| username | varchar | 15   | 0      | 是   | 用户名 |      |
+
+#### 3. 图书分类表class_info
+| 名         | 类型    | 长度 | 小数点 | NULL | 用途   | 键   |
+| :--------- | :------ | ---- | ------ | ---- | ------ | ---- |
+| class_id   | int     | 11   | 0      | 否   | 类别号 | ✔    |
+| class_name | varchar | 15   | 0      | 否   | 类别名 |      |
+
+#### 4. 借阅信息表lend_list
+| 名        | 类型   | 长度 | 小数点 | NULL | 用途     | 键   |
+| :-------- | :----- | ---- | ------ | ---- | -------- | ---- |
+| ser_num   | bigint | 20   | 0      | 否   | 流水号   | ✔    |
+| book_id   | bigint | 20   | 0      | 否   | 图书号   |      |
+| reader_id | bigint | 20   | 0      | 否   | 读者证号 |      |
+| lend_date | date   | 0    | 0      | 是   | 借出日期 |      |
+| back_date | date   | 0    | 0      | 是   | 归还日期 |      |
+
+#### 5. 借阅卡信息表reader_card
+| 名        | 类型    | 长度 | 小数点 | NULL | 用途     | 键   |
+| :-------- | :------ | ---- | ------ | ---- | -------- | ---- |
+| reader_id | bigint  | 20   | 0      | 否   | 读者证号 | ✔    |
+| password  | varchar | 15   | 0      | 否   | 密码     |      |
+| username  | varchar | 15   | 0      | 是   | 用户名   |      |
+
+#### 6. 读者信息表reader_info
+| 名        | 类型    | 长度 | 小数点 | NULL | 用途     | 键   |
+| :-------- | :------ | ---- | ------ | ---- | -------- | ---- |
+| reader_id | bigint  | 20   | 0      | 否   | 读者证号 | ✔    |
+| name      | varchar | 10   | 0      | 否   | 姓名     |      |
+| sex       | varchar | 2    | 0      | 否   | 性别     |      |
+| birth     | date    | 0    | 0      | 否   | 生日     |      |
+| address   | varchar | 50   | 0      | 否   | 地址     |      |
+| phone     | varchar | 15   | 0      | 否   | 电话     |      |
+
+### 功能展示
+#### 1.	首页登陆
+管理者账号：123456/123456
+读者账号：10000/123456
+<img src="./preview/5.png">
+
+#### 2.	管理员系统
+用登陆进入
+##### 2.1 图书管理
+<img src="./preview/6.png">
+
+##### 2.2 图书详情
+<img src="./preview/7.png">
+
+##### 2.3 读者管理
+<img src="./preview/8.png">
+
+##### 2.4 借还管理
+<img src="./preview/9.png">
+
+#### 3.	读者系统
+##### 3.1 查看全部图书
+<img src="./preview/10.png">
+
+##### 3.2 个人信息查看，可以修个个人信息
+<img src="./preview/11.png">
+
+##### 3.3 个人借阅情况查看
+<img src="./preview/12.png">
+
+
---
+++ src/main/java/com/library/controller/LoginController.java
@@ -0,0 +1,151 @@
+package com.library.controller;
+
+import com.library.bean.Admin;
+import com.library.bean.ReaderCard;
+import com.library.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+
+@Controller
+public class LoginController {
+
+    private LoginService loginService;
+
+
+/*
+* @Autowired 注释，它可以对类成员变量、方法及构造函数进行标注，完成自动装配的工作。
+*  通过 @Autowired的使用来消除 set ，get方法。
+* */
+    @Autowired
+    public void setLoginService(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+//在Spring MVC 中使用 @RequestMapping 来映射请求,跳转至相应的jsp界面
+    @RequestMapping(value = {"/", "/login.html"})
+    public String toLogin(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "index";
+    }
+
+    @RequestMapping("/logout.html")
+    public String logout(HttpServletRequest request) {
+        //用于清除session的所有信息,通常用于注销用户
+        request.getSession().invalidate();
+        return "redirect:/login.html";
+    }
+
+
+    //负责处理loginCheck.html请求
+    //请求参数会根据参数名称默认契约自动绑定到相应方法的入参中
+    @RequestMapping(value = "/api/loginCheck", method = RequestMethod.POST)
+    public @ResponseBody
+    Object loginCheck(HttpServletRequest request) {
+        long id = Long.parseLong(request.getParameter("id"));
+        String passwd = request.getParameter("passwd");
+        //与数据库中查询到的相匹配，若正确，返回true,反之返回false
+        boolean isReader = loginService.hasMatchReader(id, passwd);
+        boolean isAdmin = loginService.hasMatchAdmin(id, passwd);
+        HashMap<String, String> res = new HashMap<>();
+        if (isAdmin) {
+            Admin admin = new Admin();
+            admin.setAdminId(id);
+            admin.setPassword(passwd);
+            String username = loginService.getAdminUsername(id);
+            admin.setUsername(username);
+            //设置session会话，管理员只需登录一次，即可进行多界面操作。
+            request.getSession().setAttribute("admin", admin);
+            //设置登录状态，管理员为1，读者为2
+            res.put("stateCode", "1");
+            res.put("msg", "管理员登陆成功！");
+        } else if (isReader) {
+            ReaderCard readerCard = loginService.findReaderCardByReaderId(id);
+            //设置session会话，读者只需登录一次，即可进行多界面操作。
+            request.getSession().setAttribute("readercard", readerCard);
+            res.put("stateCode", "2");
+            res.put("msg", "读者登陆成功！");
+        } else {
+            res.put("stateCode", "0");
+            res.put("msg", "账号或密码错误！");
+        }
+        return res;
+    }
+
+    @RequestMapping("/admin_main.html")
+    public ModelAndView toAdminMain(HttpServletResponse response) {
+        return new ModelAndView("admin_main");
+    }
+
+    @RequestMapping("/reader_main.html")
+    public ModelAndView toReaderMain(HttpServletResponse response) {
+        return new ModelAndView("reader_main");
+    }
+    //在Spring MVC 中使用 @RequestMapping 来映射请求,跳转至相应的jsp界面
+    @RequestMapping("/admin_repasswd.html")
+    public ModelAndView reAdminPasswd() {
+        return new ModelAndView("admin_repasswd");
+    }
+
+    @RequestMapping("/admin_repasswd_do")
+    public String reAdminPasswdDo(HttpServletRequest request, String oldPasswd, String newPasswd, String reNewPasswd, RedirectAttributes redirectAttributes) {
+        //得到登录时设置的session，
+        Admin admin = (Admin) request.getSession().getAttribute("admin");
+        long id = admin.getAdminId();
+        //获取当前管理员的密码
+        String password = loginService.getAdminPassword(id);
+        if (password.equals(oldPasswd)) {
+            //若用户输入的旧密码与数据库中的相匹配，则修改成功
+            if (loginService.adminRePassword(id, newPasswd)) {
+                redirectAttributes.addFlashAttribute("succ", "密码修改成功！");
+                //重定向至修改密码界面
+                return "redirect:/admin_repasswd.html";
+            } else {
+                redirectAttributes.addFlashAttribute("error", "密码修改失败！");
+                return "redirect:/admin_repasswd.html";
+            }
+        } else {
+            redirectAttributes.addFlashAttribute("error", "旧密码错误！");
+            return "redirect:/admin_repasswd.html";
+        }
+    }
+
+    @RequestMapping("/reader_repasswd.html")
+    public ModelAndView reReaderPasswd() {
+        return new ModelAndView("reader_repasswd");
+    }
+
+    @RequestMapping("/reader_repasswd_do")
+    public String reReaderPasswdDo(HttpServletRequest request, String oldPasswd, String newPasswd, String reNewPasswd, RedirectAttributes redirectAttributes) {
+        ReaderCard reader = (ReaderCard) request.getSession().getAttribute("readercard");
+        long id = reader.getReaderId();
+        String password = loginService.getReaderPassword(id);
+        if (password.equals(oldPasswd)) {
+            if (loginService.readerRePassword(id, newPasswd)) {
+                redirectAttributes.addFlashAttribute("succ", "密码修改成功！");
+                return "redirect:/reader_repasswd.html";
+            } else {
+                redirectAttributes.addFlashAttribute("error", "密码修改失败！");
+                return "redirect:/reader_repasswd.html";
+            }
+        } else {
+            redirectAttributes.addFlashAttribute("error", "旧密码错误！");
+            return "redirect:/reader_repasswd.html";
+        }
+    }
+
+    //配置404页面
+    @RequestMapping("*")
+    public String notFind() {
+        return "404";
+    }
+
+}
